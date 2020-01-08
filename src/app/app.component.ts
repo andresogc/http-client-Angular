a6b7457602bfea3d18e3f941df057a1ba6bbb3df
andresogc <andresogc2011@gmail.com>
2020-01-08 10:08:26 -0500

    Conectando a una rest api en angular usando httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TaskService } from "./services/task.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ANGULAR-HTTP-CLIENT';
+
+  constructor(private taskService:TaskService){}
+
+  getAllTasks(){
+    this.taskService.getAllTasks()
+    .subscribe(tasks => {
+      console.log(tasks);
+    });
+  }
+
+  getTask(){
+    this.taskService.getTask('1')
+    .subscribe(task => {
+      console.log(task);
+    });
+  }
+
+  createTask(){
+    const task ={
+
+      userId:'1',
+      title:'change title',
+      completed:true
+    };
+    this.taskService.createTask(task)
+    .subscribe((newTask)=> {
+      console.log(newTask);
+    });
+  }
+
+  updateTask(){
+    const task ={
+      id:'199',
+      userId:'1',
+      title:'por otro title',
+      completed:true
+    };
+    this.taskService.updateTask(task)
+    .subscribe((newTask)=> {
+      console.log(newTask);
+    });
+  }
+
+  deleteTask(){
+    this.taskService.deleteTask('1')
+    .subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+
 }
